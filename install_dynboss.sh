43d480a05f156018df772d4c0b7bf5bef39ae700
Igor Martayan <igor@martayan.org>
2024-01-16 11:46:24 +0100

    Add install_dynboss.sh

---
+++ install_dynboss.sh
@@ -0,0 +1,11 @@
+git submodule update --init --recursive
+cd dynboss
+cd dsk-1.6906
+make dsk canon=0
+# make dsk k=64 canon=0
+cd ..
+cd src
+sudo apt install -y libboost-all-dev
+sudo apt install -y libtclap-dev
+sudo apt install -y libsdsl-dev
+make revcomps=0
