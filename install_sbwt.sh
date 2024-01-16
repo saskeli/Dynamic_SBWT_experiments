14f157b672e509d6c470757f06030776d0a73d02
Igor Martayan <igor@martayan.org>
2024-01-16 16:18:52 +0100

    Don't use g++-8 to install sbwt

--- install_sbwt.sh
+++ install_sbwt.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 git submodule update --init --recursive
 cd SBWT
 cd build
-cmake .. -DCMAKE_CXX_COMPILER=g++-8 -DMAX_KMER_LENGTH=32
+cmake .. -DCMAKE_CXX_COMPILER=g++ -DMAX_KMER_LENGTH=32
 make -j8
