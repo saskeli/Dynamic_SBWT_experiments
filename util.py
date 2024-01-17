896d17d54216a795bb989bb40a2c23f61e77cd63
Igor Martayan <igor@martayan.org>
2024-01-17 11:52:51 +0100

    Add util.py to run commands and measure performances

---
+++ util.py
@@ -0,0 +1,37 @@
+# Adapted from https://github.com/jnalanko/SBWT_experiments/blob/master/setup.py
+
+import subprocess
+import sys
+import os
+
+
+ENV = os.environ.copy()
+GNU_TIME = "/usr/bin/time"
+
+
+def run_cmd(command):
+    sys.stderr.write(command + "\n")
+    return (
+        subprocess.run(command, shell=True, stdout=subprocess.PIPE, env=ENV)
+        .stdout.decode("utf-8")
+        .strip()
+    )
+
+
+def measure_time(command):
+    """
+    Measure time (in s) and memory usage (in KB) of a command
+    """
+    sys.stderr.write(command + "\n")
+    result = (
+        subprocess.run(
+            f"{GNU_TIME} -f '%e %M' {command}",
+            shell=True,
+            stderr=subprocess.PIPE,
+            env=ENV,
+        )
+        .stderr.decode("utf-8")
+        .strip()
+    )
+    (time_s, mem_kb) = result.splitlines()[-1].split()
+    return float(time_s), int(mem_kb)
