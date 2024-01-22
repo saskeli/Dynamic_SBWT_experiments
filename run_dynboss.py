86fd64bb1a6f0bc04e5c70453f0d100e4a7ba938
Igor Martayan <igor@martayan.org>
2024-01-22 14:48:47 +0100

    Fix dynboss memory measure

--- run_dynboss.py
+++ run_dynboss.py
@@ -9,7 +9,7 @@ def build(fasta_file, k):
     time1, mem1 = measure_time(f"./dynboss/dsk-1.6906/dsk {fasta_file} {k} -o {prefix}")
     time2, mem2 = measure_time(f"./dynboss/bin/cosmo-pack {count_file} -o {prefix}")
     time3, mem3 = measure_time(f"./dynboss/bin/dynamicBOSS build -p {packed_file}")
-    return time1 + time2 + time3, mem1 + mem2 + mem3
+    return time1 + time2 + time3, max(mem1, mem2, mem3)
 
 
 def clean(fasta_file):
