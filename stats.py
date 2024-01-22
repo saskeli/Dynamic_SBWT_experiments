3139ac228ec09bf9f923702af926ec3b678f78c3
Igor Martayan <igor@martayan.org>
2024-01-22 11:17:46 +0100

    Update stats to store count

--- stats.py
+++ stats.py
@@ -19,17 +19,18 @@ def stats_filename(prefix, fasta_file, k):
 
 def build_stats(fasta_file, k):
     data = {"file": fasta_file}
-    data["size"] = util.get_filesize(fasta_file)
-    time, mem = run_bifrost.build(fasta_file, k)
-    data["Bifrost"] = {"time": time, "mem": mem}
     time, mem = run_cbl.build(fasta_file, k)
     data["CBL"] = {"time": time, "mem": mem}
-    time, mem = run_dynboss.build(fasta_file, k)
-    data["DynamicBOSS"] = {"time": time, "mem": mem}
-    time, mem = run_sbwt.build(fasta_file, k)
-    data["SBWT"] = {"time": time, "mem": mem}
     time, mem = run_sshash.build(fasta_file, k)
     data["SSHash"] = {"time": time, "mem": mem}
+    time, mem = run_sbwt.build(fasta_file, k)
+    data["SBWT"] = {"time": time, "mem": mem}
+    time, mem = run_bifrost.build(fasta_file, k)
+    data["Bifrost"] = {"time": time, "mem": mem}
+    time, mem = run_dynboss.build(fasta_file, k)
+    data["DynamicBOSS"] = {"time": time, "mem": mem}
+    data["bytes"] = util.get_filesize(fasta_file)
+    data["count"] = run_cbl.count(fasta_file, k)
     output = stats_filename("build", fasta_file, k)
     with open(output, "w+") as f:
         json.dump(data, f)
