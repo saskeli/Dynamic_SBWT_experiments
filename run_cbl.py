83f1c359b2653a997c917c8931aca819faa31314
Igor Martayan <igor@martayan.org>
2024-01-22 15:42:13 +0100

    Remove k in count, insert, remove

--- run_cbl.py
+++ run_cbl.py
@@ -12,31 +12,28 @@ def build(fasta_file, k):
     )
 
 
-def count(fasta_file, k):
-    prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
+def count(indexed_file):
+    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     cbl_file = prefix + ".cbl"
-    run_cmd(f"cd CBL && K={k} cargo +nightly build --release --examples")
     out, _ = run_cmd(
         f"./CBL/target/release/examples/index_count {cbl_file} 2>&1 | tail -n 1 | cut -d ' ' -f 3"
     )
     return int(out)
 
 
-def insert(indexed_file, query_file, k):
+def insert(indexed_file, query_file):
     prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     cbl_file = prefix + ".cbl"
     updated_file = f"{prefix}_add_{get_basename(query_file)}.cbl"
-    run_cmd(f"cd CBL && K={k} cargo +nightly build --release --examples")
     return measure_time(
         f"./CBL/target/release/examples/insert_index {cbl_file} {query_file} -o {updated_file}"
     )
 
 
-def remove(indexed_file, query_file, k):
+def remove(indexed_file, query_file):
     prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     cbl_file = prefix + ".cbl"
     updated_file = f"{prefix}_rem_{get_basename(query_file)}.cbl"
-    run_cmd(f"cd CBL && K={k} cargo +nightly build --release --examples")
     return measure_time(
         f"./CBL/target/release/examples/remove_index {cbl_file} {query_file} -o {updated_file}"
     )
