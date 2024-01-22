77d2b05632d9c64243eb53be2a1103ac050a356b
Igor Martayan <igor@martayan.org>
2024-01-22 14:08:39 +0100

    Improve run_cmd to handle exceptions, use builtin timeout

--- util.py
+++ util.py
@@ -6,28 +6,40 @@ import sys
 import os
 
 
-ENV = os.environ.copy()
 GNU_TIME = "/usr/bin/time"
-TIMEOUT = 120
+ENV = os.environ.copy()
 OUT_FOLDER = "out"
+TIMEOUT = 120
 
 
-def run_cmd(command):
+def run_cmd(command, timeout=None):
     sys.stderr.write("> " + command + "\n")
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=ENV)
-    out, err = proc.communicate()
-    out = out.decode("utf-8").strip()
-    err = err.decode("utf-8").strip()
-    sys.stdout.write(out + "\n")
-    sys.stderr.write(err + "\n")
-    return out, err
+    try:
+        proc = subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            timeout=timeout,
+            capture_output=True,
+            text=True,
+            env=ENV,
+        )
+        out, err = proc.stdout.strip(), proc.stderr.strip()
+        sys.stdout.write(out + "\n")
+        sys.stderr.write(err + "\n")
+        return out, err
+    except subprocess.SubprocessError as proc:
+        out, err = proc.stdout.strip(), proc.stderr.strip()
+        sys.stdout.write(out + "\n")
+        sys.stderr.write(err + "\n")
+        return None, None
 
 
 def measure_time(command, timeout=TIMEOUT):
     """
     Measure time (in s) and memory usage (in KB) of a command
     """
-    _, err = run_cmd(f"timeout {timeout} {GNU_TIME} -f '%e %M' {command}")
+    _, err = run_cmd(f"{GNU_TIME} -f '%e %M' {command}", timeout=timeout)
     try:
         time_s, mem_kb = err.splitlines()[-1].split()
         return float(time_s), int(mem_kb)
