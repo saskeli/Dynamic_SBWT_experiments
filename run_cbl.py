2b9cafc01266b3cc4907101dff5268702babf1c3
Igor Martayan <igor@martayan.org>
2024-01-22 14:59:23 +0100

    Fix K in run_cbl

--- run_cbl.py
+++ run_cbl.py
@@ -6,7 +6,7 @@ def build(fasta_file, k):
     os.makedirs(OUT_FOLDER, exist_ok=True)
     prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
     cbl_file = prefix + ".cbl"
-    run_cmd(f"K={k} cd CBL && cargo +nightly build --release --examples")
+    run_cmd(f"cd CBL && K={k} cargo +nightly build --release --examples")
     return measure_time(
         f"./CBL/target/release/examples/build_index {fasta_file} -o {cbl_file}"
     )
@@ -15,7 +15,7 @@ def build(fasta_file, k):
 def count(fasta_file, k):
     prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
     cbl_file = prefix + ".cbl"
-    run_cmd(f"K={k} cd CBL && cargo +nightly build --release --examples")
+    run_cmd(f"cd CBL && K={k} cargo +nightly build --release --examples")
     out, _ = run_cmd(
         f"./CBL/target/release/examples/index_count {cbl_file} 2>&1 | tail -n 1 | cut -d ' ' -f 3"
     )
