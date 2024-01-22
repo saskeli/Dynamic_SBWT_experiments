0e0a921cc2eb51d7986c3ced8571803e800bbc4b
Igor Martayan <igor@martayan.org>
2024-01-22 13:49:02 +0100

    Improve run_cmd to show output while running, add timeout when measuring runtime

--- util.py
+++ util.py
@@ -8,40 +8,37 @@ import os
 
 ENV = os.environ.copy()
 GNU_TIME = "/usr/bin/time"
+TIMEOUT = 120
+OUT_FOLDER = "out"
 
 
 def run_cmd(command):
-    sys.stderr.write(command + "\n")
-    return (
-        subprocess.run(command, shell=True, stdout=subprocess.PIPE, env=ENV)
-        .stdout.decode("utf-8")
-        .strip()
-    )
+    sys.stderr.write("> " + command + "\n")
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=ENV)
+    out, err = proc.communicate()
+    out = out.decode("utf-8").strip()
+    err = err.decode("utf-8").strip()
+    sys.stdout.write(out + "\n")
+    sys.stderr.write(err + "\n")
+    return out, err
 
 
-def measure_time(command):
+def measure_time(command, timeout=TIMEOUT):
     """
     Measure time (in s) and memory usage (in KB) of a command
     """
-    sys.stderr.write(command + "\n")
-    result = (
-        subprocess.run(
-            f"{GNU_TIME} -f '%e %M' {command}",
-            shell=True,
-            stderr=subprocess.PIPE,
-            env=ENV,
-        )
-        .stderr.decode("utf-8")
-        .strip()
-    )
-    time_s, mem_kb = result.splitlines()[-1].split()
-    return float(time_s), int(mem_kb)
+    _, err = run_cmd(f"timeout {timeout} {GNU_TIME} -f '%e %M' {command}")
+    try:
+        time_s, mem_kb = err.splitlines()[-1].split()
+        return float(time_s), int(mem_kb)
+    except Exception:
+        return None, None
 
 
 def get_filesize(filename):
     if filename.endswith(".gz"):
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             f.seek(-4, 2)
-            return struct.unpack('I', f.read(4))[0]
+            return struct.unpack("I", f.read(4))[0]
     else:
         return os.path.getsize(filename)
