0d8b9df93ff3f0f572dec158754234bca7e3c1bf
Igor Martayan <igor@martayan.org>
2024-01-23 14:58:24 +0100

    Fix SBWT query

--- run_sbwt.py
+++ run_sbwt.py
@@ -15,7 +15,7 @@ def query(indexed_file, query_file):
     prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
     sbwt_file = prefix + ".sbwt"
     return measure_time(
-        f"./SBWT/build/bin/sbwt search -i {sbwt} -q {query_file} -o /dev/null"
+        f"./SBWT/build/bin/sbwt search -i {sbwt_file} -q {query_file} -o /dev/null"
     )
 
 
