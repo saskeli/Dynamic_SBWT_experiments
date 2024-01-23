ab7fe6ce953ecefe6ebb8a16e4a64f83729d527d
Igor Martayan <igor@martayan.org>
2024-01-23 17:35:51 +0100

    Add stats for query, insert and remove

--- stats.py
+++ stats.py
@@ -48,23 +48,84 @@ def query_filename(prefix, indexed_file, query_file):
     return f"{DATA_FOLDER}/{prefix}_{size}_{index_basename}_{query_basename}.json"
 
 
-def query_self_stats(fasta_file):
+def query_self_stats(indexed_file):
     os.makedirs(DATA_FOLDER, exist_ok=True)
-    output = query_filename("query_self", fasta_file, fasta_file)
-    data = {"file": fasta_file}
-    time, mem = run_cbl.query(fasta_file, fasta_file)
+    output = query_filename("query_self", indexed_file, indexed_file)
+    data = {"file": indexed_file}
+    time, mem = run_cbl.query(indexed_file, indexed_file)
     data["CBL"] = {"time": time, "mem": mem}
-    time, mem = run_sshash.query(fasta_file, fasta_file)
+    time, mem = run_sshash.query(indexed_file, indexed_file)
     data["SSHash"] = {"time": time, "mem": mem}
-    time, mem = run_sbwt.query(fasta_file, fasta_file)
+    time, mem = run_sbwt.query(indexed_file, indexed_file)
     data["SBWT"] = {"time": time, "mem": mem}
-    time, mem = run_bifrost.query(fasta_file, fasta_file)
+    time, mem = run_bifrost.query(indexed_file, indexed_file)
     data["Bifrost"] = {"time": time, "mem": mem}
-    time, mem = run_bufboss.query(fasta_file, fasta_file)
+    time, mem = run_bufboss.query(indexed_file, indexed_file)
     data["BufBOSS"] = {"time": time, "mem": mem}
-    time, mem = run_dynboss.query(fasta_file, fasta_file)
-    data["DynamicBOSS"] = {"time": time, "mem": mem}
-    data["kmers"] = run_cbl.count(fasta_file)
-    data["bytes"] = get_filesize(fasta_file)
+    # time, mem = run_dynboss.query(indexed_file, indexed_file)
+    # data["DynamicBOSS"] = {"time": time, "mem": mem}
+    data["kmers"] = run_cbl.count(indexed_file)
+    data["bytes"] = get_filesize(indexed_file)
+    with open(output, "w+") as f:
+        json.dump(data, f)
+
+
+def query_other_stats(indexed_file, query_file):
+    os.makedirs(DATA_FOLDER, exist_ok=True)
+    output = query_filename("query_other", indexed_file, query_file)
+    data = {"file": indexed_file, "query_file": query_file}
+    time, mem = run_cbl.query(indexed_file, query_file)
+    data["CBL"] = {"time": time, "mem": mem}
+    time, mem = run_sshash.query(indexed_file, query_file)
+    data["SSHash"] = {"time": time, "mem": mem}
+    time, mem = run_sbwt.query(indexed_file, query_file)
+    data["SBWT"] = {"time": time, "mem": mem}
+    time, mem = run_bifrost.query(indexed_file, query_file)
+    data["Bifrost"] = {"time": time, "mem": mem}
+    time, mem = run_bufboss.query(indexed_file, query_file)
+    data["BufBOSS"] = {"time": time, "mem": mem}
+    # time, mem = run_dynboss.query(indexed_file, query_file)
+    # data["DynamicBOSS"] = {"time": time, "mem": mem}
+    data["kmers"] = run_cbl.count(indexed_file)
+    data["bytes"] = get_filesize(indexed_file)
+    data["query_bytes"] = get_filesize(query_file)
+    with open(output, "w+") as f:
+        json.dump(data, f)
+
+
+def insert_stats(indexed_file, query_file):
+    os.makedirs(DATA_FOLDER, exist_ok=True)
+    output = query_filename("insert", indexed_file, query_file)
+    data = {"file": indexed_file, "query_file": query_file}
+    time, mem = run_cbl.insert(indexed_file, query_file)
+    data["CBL"] = {"time": time, "mem": mem}
+    time, mem = run_bifrost.insert(indexed_file, query_file)
+    data["Bifrost"] = {"time": time, "mem": mem}
+    time, mem = run_bufboss.insert(indexed_file, query_file)
+    data["BufBOSS"] = {"time": time, "mem": mem}
+    # time, mem = run_dynboss.insert(indexed_file, query_file)
+    # data["DynamicBOSS"] = {"time": time, "mem": mem}
+    data["kmers"] = run_cbl.count(indexed_file)
+    data["bytes"] = get_filesize(indexed_file)
+    data["query_kmers"] = run_cbl.count_query(indexed_file, query_file)
+    data["query_bytes"] = get_filesize(query_file)
+    with open(output, "w+") as f:
+        json.dump(data, f)
+
+
+def remove_stats(indexed_file, query_file):
+    os.makedirs(DATA_FOLDER, exist_ok=True)
+    output = query_filename("remove", indexed_file, query_file)
+    data = {"file": indexed_file, "query_file": query_file}
+    time, mem = run_cbl.remove(indexed_file, query_file)
+    data["CBL"] = {"time": time, "mem": mem}
+    time, mem = run_bufboss.remove(indexed_file, query_file)
+    data["BufBOSS"] = {"time": time, "mem": mem}
+    # time, mem = run_dynboss.remove(indexed_file, query_file)
+    # data["DynamicBOSS"] = {"time": time, "mem": mem}
+    data["kmers"] = run_cbl.count(indexed_file)
+    data["bytes"] = get_filesize(indexed_file)
+    data["query_kmers"] = run_cbl.count_query(indexed_file, query_file)
+    data["query_bytes"] = get_filesize(query_file)
     with open(output, "w+") as f:
         json.dump(data, f)
