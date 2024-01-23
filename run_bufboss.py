97d3904acb167c457ff15c540e4b2e88478f22c9
Igor Martayan <igor@martayan.org>
2024-01-23 17:18:41 +0100

    Add insert and remove to run_bufboss

--- run_bufboss.py
+++ run_bufboss.py
@@ -25,6 +25,28 @@ def query(indexed_file, query_file):
     return measure_time(f"./bufboss/bin/bufboss_query -i {bufboss_folder} -q {query_file} -o /dev/null")
 
 
+def insert(indexed_file, query_file):
+    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
+    bufboss_folder = prefix + "_bufboss"
+    updated_folder = f"{prefix}_add_{get_basename(query_file)}_bufboss"
+    if query_file.endswith(".gz"):
+        unzipped_file = f"{OUT_FOLDER}/{get_basename(query_file)}"
+        run_cmd(f"gzip -cd {query_file} > {unzipped_file}")
+        query_file = unzipped_file
+    return measure_time(f"./bufboss/bin/bufboss_update -i {bufboss_folder} -a {query_file} -o {updated_folder}")
+
+
+def remove(indexed_file, query_file):
+    prefix = f"{OUT_FOLDER}/{get_basename(indexed_file)}"
+    bufboss_folder = prefix + "_bufboss"
+    updated_folder = f"{prefix}_rem_{get_basename(query_file)}_bufboss"
+    if query_file.endswith(".gz"):
+        unzipped_file = f"{OUT_FOLDER}/{get_basename(query_file)}"
+        run_cmd(f"gzip -cd {query_file} > {unzipped_file}")
+        query_file = unzipped_file
+    return measure_time(f"./bufboss/bin/bufboss_update -i {bufboss_folder} -d {query_file} -o {updated_folder}")
+
+
 def clean(fasta_file):
     prefix = f"{OUT_FOLDER}/{get_basename(fasta_file)}"
     bufboss_folder = prefix + "_bufboss"
