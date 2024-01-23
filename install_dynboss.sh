d8f4180db63f7f13086670cd9fe838dcd83f8e24
Igor Martayan <igor@martayan.org>
2024-01-23 16:27:26 +0100

    Fix python version when installing dynboss

--- install_dynboss.sh
+++ install_dynboss.sh
@@ -3,6 +3,9 @@ set -euxo pipefail
 
 git submodule update --init --recursive
 cd dynboss
+sed -i -e 's/python/python3/g' src/Makefile
+sed -i -e 's/print /print(/g' src/make_lut.py
+sed -i -e 's/))))/)))))/g' src/make_lut.py
 cd dsk-1.6906
 make dsk canon=0
 # make dsk k=64 canon=0
