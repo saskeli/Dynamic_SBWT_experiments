9c5a6cb3a8539d58b0db8b7720e08edbf65d1735
Igor Martayan <igor@martayan.org>
2024-01-25 07:38:55 +0100

    Factorize plot functions

--- plots.py
+++ plots.py
@@ -8,11 +8,9 @@ import shutil
 PLOT_FOLDER = "plot"
 PLOT_FORMAT = ".png"
 FONT_SIZE = 12
-DATA_FILES = os.listdir(DATA_FOLDER)
 SINGLE_TASKS = ["build", "query_self"]
 QUERY_TASKS = ["query_other", "insert", "remove"]
 TASKS = SINGLE_TASKS + QUERY_TASKS
-DATA = {task: [] for task in TASKS}
 TOOLS = {
     "build": ["CBL", "SSHash", "SBWT", "Bifrost", "BufBOSS", "DynamicBOSS", "HashSet"],
     "size": ["CBL", "SSHash", "SBWT", "Bifrost", "DynamicBOSS", "HashSet"],
@@ -63,12 +61,8 @@ LABEL["remove"] = {
     "query_bytes": "Query size (in bytes)",
     "query_kmers": "# deleted $k$-mers",
 }
-
-
-def ncol(n):
-    if n <= 3:
-        return n
-    return (n + 1) // 2
+DATA_FILES = os.listdir(DATA_FOLDER)
+DATA = {task: [] for task in TASKS}
 
 
 for filename in DATA_FILES:
@@ -86,21 +80,30 @@ for filename in DATA_FILES:
                 DATA["remove"].append(json.load(f))
 
 
-def plot_time_bytes(task):
-    xkey = "bytes" if task in SINGLE_TASKS else "query_bytes"
-    prefix = f"{PLOT_FOLDER}/plot_{task}_time_bytes"
+def plot_task(task, ykey, xkey, name=None):
+    if name is not None:
+        prefix = f"{PLOT_FOLDER}/{name}"
+    elif ykey == "size":
+        prefix = f"{PLOT_FOLDER}/plot_size_{xkey.split('_')[-1]}"
+    elif ykey == "mem":
+        prefix = f"{PLOT_FOLDER}/plot_{task}_ram_{xkey.split('_')[-1]}"
+    else:
+        prefix = f"{PLOT_FOLDER}/plot_{task}_{ykey}_{xkey.split('_')[-1]}"
     plt.rcParams.update({"font.size": FONT_SIZE})
     fig, ax = plt.subplots()
     for tool in TOOLS[task]:
         X, Y = [], []
         for d in DATA[task]:
-            if tool in d:
+            if tool in d and ykey in d[tool]:
                 X.append(d[xkey])
-                Y.append(d[tool]["time"])
+                if ykey == "mem":
+                    Y.append(d[tool][ykey] / 1000)
+                else:
+                    Y.append(d[tool][ykey])
         ax.scatter(X, Y, label=LABEL[tool], marker=MARKER[tool], alpha=0.5)
     ax.set_yscale("log")
     ax.set_xscale("log")
-    ax.set_ylabel(LABEL[task]["time"])
+    ax.set_ylabel(LABEL[task][ykey])
     ax.set_xlabel(LABEL[task][xkey])
     ax.legend(
         loc="lower center",
@@ -111,85 +114,20 @@ def plot_time_bytes(task):
     plt.savefig(prefix + PLOT_FORMAT, bbox_inches="tight", dpi=300)
 
 
-def plot_time_kmers(task):
-    xkey = "kmers" if task in SINGLE_TASKS else "query_kmers"
-    prefix = f"{PLOT_FOLDER}/plot_{task}_time_kmers"
-    plt.rcParams.update({"font.size": FONT_SIZE})
-    fig, ax = plt.subplots()
-    for tool in TOOLS[task]:
-        X, Y = [], []
-        for d in DATA[task]:
-            if tool in d:
-                X.append(d[xkey])
-                Y.append(d[tool]["time"])
-        ax.scatter(X, Y, label=LABEL[tool], marker=MARKER[tool], alpha=0.5)
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    ax.set_ylabel(LABEL[task]["time"])
-    ax.set_xlabel(LABEL[task][xkey])
-    ax.legend(
-        loc="lower center",
-        bbox_to_anchor=(0.5, 1.025),
-        ncol=ncol(len(TOOLS[task])),
-    )
-    os.makedirs(PLOT_FOLDER, exist_ok=True)
-    plt.savefig(prefix + PLOT_FORMAT, bbox_inches="tight", dpi=300)
-
-
-def plot_ram_kmers(task):
-    xkey = "kmers" if task in SINGLE_TASKS else "query_kmers"
-    prefix = f"{PLOT_FOLDER}/plot_{task}_ram_kmers"
-    plt.rcParams.update({"font.size": FONT_SIZE})
-    fig, ax = plt.subplots()
-    for tool in TOOLS[task]:
-        X, Y = [], []
-        for d in DATA[task]:
-            if tool in d:
-                X.append(d[xkey])
-                Y.append(d[tool]["mem"]) / 1000
-        ax.scatter(X, Y, label=LABEL[tool], marker=MARKER[tool], alpha=0.5)
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    ax.set_ylabel(LABEL[task]["mem"])
-    ax.set_xlabel(LABEL[task][xkey])
-    ax.legend(
-        loc="lower center",
-        bbox_to_anchor=(0.5, 1.025),
-        ncol=ncol(len(TOOLS[task])),
-    )
-    os.makedirs(PLOT_FOLDER, exist_ok=True)
-    plt.savefig(prefix + PLOT_FORMAT, bbox_inches="tight", dpi=300)
-
-
-def plot_size_kmers():
-    xkey = "kmers"
-    prefix = f"{PLOT_FOLDER}/plot_size_kmers"
-    plt.rcParams.update({"font.size": FONT_SIZE})
-    fig, ax = plt.subplots()
-    for tool in TOOLS["size"]:
-        X, Y = [], []
-        for d in DATA["build"]:
-            if tool in d:
-                X.append(d[xkey])
-                Y.append(d[tool]["size"])
-        ax.scatter(X, Y, label=LABEL[tool], marker=MARKER[tool], alpha=0.5)
-    ax.set_yscale("log")
-    ax.set_xscale("log")
-    ax.set_ylabel(LABEL["build"]["size"])
-    ax.set_xlabel(LABEL["build"][xkey])
-    ax.legend(
-        loc="lower center",
-        bbox_to_anchor=(0.5, 1.025),
-        ncol=ncol(len(TOOLS[task])),
-    )
-    os.makedirs(PLOT_FOLDER, exist_ok=True)
-    plt.savefig(prefix + PLOT_FORMAT, bbox_inches="tight", dpi=300)
+def ncol(n):
+    if n <= 3:
+        return n
+    return (n + 1) // 2
 
 
 if __name__ == "__main__":
-    for task in TASKS:
-        plot_time_bytes(task)
-        plot_time_kmers(task)
-        plot_ram_kmers(task)
-    plot_size_kmers()
+    for task in SINGLE_TASKS:
+        plot_task(task, "time", "bytes")
+        plot_task(task, "time", "kmers")
+        plot_task(task, "mem", "kmers")
+    for task in QUERY_TASKS:
+        plot_task(task, "time", "query_bytes")
+        plot_task(task, "time", "query_kmers")
+        plot_task(task, "mem", "query_kmers")
+    plot_task("build", "size", "kmers")
     shutil.make_archive(PLOT_FOLDER, "zip", PLOT_FOLDER)
