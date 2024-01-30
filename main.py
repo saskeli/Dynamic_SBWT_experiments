8c91e5ce8f1eb83e3aff82b23b12c0a43d001f17
Igor Martayan <igor@martayan.org>
2024-01-30 10:42:56 +0100

    Read files to build and query from a file of files

--- main.py
+++ main.py
@@ -1,102 +1,44 @@
 import util
 import stats
+import os
 
 
 K = 31
+PREFIX_BITS = 28
+FOF_BUILD = "fof_build.txt"
+FOF_QUERY = "fof_query.txt"
 util.TIMEOUT = 300
-
-BUILD = [
-    "../files_1801-1900_31/SRR972708.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR975415.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR962597.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR976738.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR953494.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR950080.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR950083.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR953488.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR972717.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR972716.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR975416.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR975412.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR962604.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR976749.unitigs.fa.gz",
-    "../files_1801-1900_31/SRR953495.unitigs.fa.gz",
-    # "../files_1801-1900_31/SRR953496.unitigs.fa.gz",
-]
-
-QUERY = [
-    (
-        "../files_1801-1900_31/SRR972708.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR972716.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR975415.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR975414.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR962597.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR950879.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR976738.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR976743.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR953494.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR950882.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR950080.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR950881.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR950083.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR950079.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR953488.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR972713.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR972717.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR972715.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR972716.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR960732.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR975416.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR960733.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR975412.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR975411.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR962604.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR962602.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR976749.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR962600.unitigs.fa.gz",
-    ),
-    (
-        "../files_1801-1900_31/SRR953495.unitigs.fa.gz",
-        "../files_1801-1900_31/SRR950084.unitigs.fa.gz",
-    ),
-    # (
-    #     "../files_1801-1900_31/SRR953496.unitigs.fa.gz",
-    #     "../files_1801-1900_31/SRR953497.unitigs.fa.gz",
-    # ),
-]
+BUILD = []
+QUERY = []
 
 if __name__ == "__main__":
+    if os.path.exists(FOF_BUILD):
+        with open(FOF_BUILD, "r") as f:
+            for line in f:
+                filename = line.strip()
+                if filename:
+                    assert os.path.exists(filename), f"Cannot find {filename}"
+                    BUILD.append(filename)
+    else:
+        print(f"Please write the FASTA files to build in {FOF_BUILD}")
+
+    if os.path.exists(FOF_QUERY):
+        with open(FOF_QUERY, "r") as f:
+            for line in f:
+                filename = line.strip()
+                if filename:
+                    assert os.path.exists(filename), f"Cannot find {filename}"
+                    QUERY.append(filename)
+    else:
+        print(f"Please write the FASTA files to query in {FOF_QUERY}")
+
     for fasta_file in BUILD:
-        stats.build_stats(fasta_file, K)
+        stats.build_stats(fasta_file, K, PREFIX_BITS)
         stats.query_self_stats(fasta_file)
 
-    for indexed_file, query_file in QUERY:
+    for indexed_file, query_file in zip(BUILD, QUERY):
         stats.query_other_stats(indexed_file, query_file)
         stats.insert_stats(indexed_file, query_file)
         stats.remove_stats(indexed_file, query_file)
+        stats.merge_stats(indexed_file, query_file)
+        stats.intersect_stats(indexed_file, query_file)
