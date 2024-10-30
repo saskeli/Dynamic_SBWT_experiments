f74ccad83a1409e24b9c6ffed6592bd62c7d569e
Saska Dönges <saskeli@kapsi.fi>
2024-10-30 05:56:23 +0200

    generalize experiment

--- build_expanding.sh
+++ build_expanding.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
-USAGE="$0 <file_list> <data_folder> <output_folder> -- build unitigs and time index creation
+USAGE="$0 <file_list> <data_folder> <output_folder> <limit> -- build unitigs and time index creation
 
 where:
     file_list    is a text file containing the names of fasta files to use.
     data_folder  is the folder containing the fasta files.
     output_foder is the directory to write unitigs and indexes to.
+    limit        maximum number of genomes to build index of. Default: 1024.
     
-All arguments are required
+All arguments, besides the limit are required
 
 Example $0 fof_build.txt data out"
 
-if [ $# -ne 3 ]; then
+if [ $# -lt 3 ]; then
     echo "$USAGE"
     exit 1
 fi
+
+BLA=$(head -n 1 $1)
+EXT=${BLA##*.}
+if [ "$EXT" = "gz" ]; then
+    FN=${BLA%.*}
+    EXT="${FN##*.}.$EXT"
+fi
+
+echo "file extenstion: $EXT"
 
 set -euxo pipefail
 
 FOF=$1
 DATA_FOLDER=$2
 OUT_FOLDER=$3
+FILE_LIMIT=$(($# > 3 ? $4 : 1024))
 
 mkdir -p ${OUT_FOLDER}
 
-for i in 2 4 8 16 32 64 128 256 512 1024; do
+i=1
+while [ $i -lt $FILE_LIMIT ]; do
+  i=$(($i * 2))
   if [ ! -f ${OUT_FOLDER}/${i}.unitigs.fa ]; then
     head -n ${i} ${FOF} | while read line ; do echo "${DATA_FOLDER}/${line}"; done > tmp.txt
     bcalm/build/bcalm -in tmp.txt -verbose 0 -kmer-size 31 -abundance-min 1 -out ${OUT_FOLDER}/${i} 
@@ -40,15 +53,19 @@ export PATH=$PATH:./bifrost/build/lib/
 MAX_THREADS=$(nproc)
 MAX_THREADS=$((MAX_THREADS > 32 ? 32 : MAX_TREADS))
 
-for i in 2 4 8 16 32 64 128 256 512 1024; do
-  echo ${OUT_FOLDER}/${i}.unitigs.fa > tmp.txt
+i=1
+while [ $i -lt $FILE_LIMIT ]; do
+  i=$(($i * 2))
+
+  FN=${OUT_FOLDER}/${i}.unitigs.fa
+  echo $FN > tmp.txt
   
-  /usr/bin/time CBL/target/release/examples/cbl build ${OUT_FOLDER}/${i}.unitigs.fa -o ${OUT_FOLDER}/${i}.cbl
-  /usr/bin/time bufboss/bin/bufboss_build -a ${OUT_FOLDER}/${i}.unitigs.fa -o ${OUT_FOLDER}/${i}.bufboss -k 31 -t tmp
-  /usr/bin/time bifrost/build/bin/Bifrost build -r ${OUT_FOLDER}/${i}.unitigs.fa -o ${OUT_FOLDER}/${i}.bifrost -k 31 -t 1
+  /usr/bin/time CBL/target/release/examples/cbl build ${FN} -o ${OUT_FOLDER}/${i}.cbl
+  /usr/bin/time bufboss/bin/bufboss_build -a ${FN} -o ${OUT_FOLDER}/${i}.bufboss -k 31 -t tmp
+  /usr/bin/time bifrost/build/bin/Bifrost build -r ${FN} -o ${OUT_FOLDER}/${i}.bifrost -k 31 -t 1
   /usr/bin/time BBB/build/bin/buffer -r -n -t 1 tmp.txt ${OUT_FOLDER}/${i}.sbwt 
   echo "threads = ${MAX_THREADS}"
-  /usr/bin/time bifrost/build/bin/Bifrost build -r ${OUT_FOLDER}/${i}.unitigs.fa -o ${OUT_FOLDER}/${i}.bifrost -k 31 -t $MAX_THREADS
+  /usr/bin/time bifrost/build/bin/Bifrost build -r ${FN} -o ${OUT_FOLDER}/${i}.bifrost -k 31 -t $MAX_THREADS
   /usr/bin/time BBB/build/bin/buffer -r -n -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_a.sbwt 
   /usr/bin/time BBB/build/bin/buffer -r -n -m 4 -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_b.sbwt 
   /usr/bin/time BBB/build/bin/buffer -r -n -m 30 -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_c.sbwt 
---
+++ build_expanding_concat.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+USAGE="$0 <file_list> <data_folder> <output_folder> <limit> -- build unitigs and time index creation
+
+where:
+    file_list    is a text file containing the names of fasta files to use.
+    data_folder  is the folder containing the fasta files.
+    output_foder is the directory to write unitigs and indexes to.
+    limit        maximum number of genomes to build index of. Default: 1024.
+    
+All arguments, besides limit are required
+
+Example $0 fof_build.txt data out"
+
+if [ $# -lt 3 ]; then
+    echo "$USAGE"
+    exit 1
+fi
+
+BLA=$(head -n 1 $1)
+EXT=${BLA##*.}
+FEXT=$EXT
+if [ "$EXT" = "gz" ]; then
+    FN=${BLA%.*}
+    FEXT=${FN##*.}
+    EXT=${FEXT}.${EXT}
+fi
+
+echo "file extenstion: $EXT"
+
+set -euxo pipefail
+
+FOF=$1
+DATA_FOLDER=$2
+OUT_FOLDER=$3
+FILE_LIMIT=$(($# > 3 ? $4 : 1024))
+
+mkdir -p ${OUT_FOLDER}
+
+i=1
+while [ $i -lt $FILE_LIMIT ]; do
+  i=$(($i * 2))
+  FN=${OUT_FOLDER}/${i}.concat.${FEXT}
+  GFN=${OUT_FOLDER}/${i}.concat.${EXT}
+  if [ ! -f $FN ]; then
+    head -n ${i} ${FOF} | while read line ; do echo "${DATA_FOLDER}/${line}"; done | xargs cat > $GFN
+    if [ "$EXT" != "$FEXT" ]; then
+      gunzip $GFN
+    fi
+  fi
+done
+
+rm -f ${OUT_FOLDER}/*glue*
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./bifrost/build/lib/
+export LIBRARY_PATH=${LIBRARY_PATH-""}:./bifrost/build/lib/
+export PATH=$PATH:./bifrost/build/lib/
+MAX_THREADS=$(nproc)
+MAX_THREADS=$((MAX_THREADS > 32 ? 32 : MAX_THREADS))
+
+i=1
+while [ $i -lt $FILE_LIMIT ]; do
+  i=$(($i * 2))
+  
+  FN=${OUT_FOLDER}/${i}.concat.${FEXT}
+  echo $FN > tmp.txt
+  
+  /usr/bin/time CBL/target/release/examples/cbl build ${FN} -o ${OUT_FOLDER}/${i}_concat.cbl
+  /usr/bin/time bufboss/bin/bufboss_build -a ${FN} -o ${OUT_FOLDER}/${i}_concat.bufboss -k 31 -t tmp
+  /usr/bin/time bifrost/build/bin/Bifrost build -r ${FN} -o ${OUT_FOLDER}/${i}_concat.bifrost -k 31 -t 1
+  /usr/bin/time BBB/build/bin/buffer -r -t 1 tmp.txt ${OUT_FOLDER}/${i}_concat.sbwt 
+  echo "threads = ${MAX_THREADS}"
+  /usr/bin/time bifrost/build/bin/Bifrost build -r ${FN} -o ${OUT_FOLDER}/${i}_concat.bifrost -k 31 -t $MAX_THREADS
+  /usr/bin/time BBB/build/bin/buffer -r -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_concat_a.sbwt 
+  /usr/bin/time BBB/build/bin/buffer -r -m 4 -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_concat_b.sbwt 
+  /usr/bin/time BBB/build/bin/buffer -r -m 10 -t $MAX_THREADS tmp.txt ${OUT_FOLDER}/${i}_concat_c.sbwt 
+
+  rm tmp.txt
+done
